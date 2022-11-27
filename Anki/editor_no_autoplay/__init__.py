4f0f0bf6efe607044070da69729e7b54ded058dc
AuroraWright <AuroraWright@users.noreply.github.com>
2022-11-27 12:28:38 +0100

    Make autoplay stop after 2s instead of removing it entirely

--- Anki/editor_no_autoplay/__init__.py
+++ Anki/editor_no_autoplay/__init__.py
@@ -1,7 +1,11 @@
 import html
 import urllib.parse
-
+from threading import Timer
+from aqt.sound import av_player
 from aqt.editor import Editor, pics
+
+def stop():
+    av_player.shutdown()
 
 def myFnameToLink(self, fname: str) -> str:
     ext = fname.split(".")[-1].lower()
@@ -9,6 +13,8 @@ def myFnameToLink(self, fname: str) -> str:
         name = urllib.parse.quote(fname.encode("utf8"))
         return f'<img src="{name}">'
     else:
+        av_player.play_file(fname)
+        Timer(2, stop).start()
         return f"[sound:{html.escape(fname, quote=False)}]"
 
 Editor.fnameToLink = myFnameToLink
